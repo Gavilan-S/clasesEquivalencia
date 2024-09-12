1ae88f79be5c2a91a67c1a32def520af76035a90
Gvln <santis.paez2414@outlook.com>
2024-09-12 17:44:00 -0500

    pruebasUnitarias: Registry casi completo

--- src/main/java/edu/unisabana/dyas/tdd/registry/Registry.java
+++ src/main/java/edu/unisabana/dyas/tdd/registry/Registry.java
@@ -1,31 +1,49 @@
 package edu.unisabana.dyas.tdd.registry;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Registry {
-  public RegisterResult registerVoter(Person person) {
+  private List<Person> personaLista;
 
-    // Edad Invalida
-
-    if(person.getAge() <= 0) {
-      throw new IllegalArgumentException("Edad invalida");
+  public Person registerVoter(Person persona) {
+    try {
+      edadRegister(persona);
+      menorRegister(persona);
+      muertoRegister(persona); 
+    } catch (Exception e) {
+      System.err.println("Error en el registro de: " + persona.getName());
     } 
-
-    // Menor de eddd
-    
-    if(person.getAge() < 18) {
-      throw new IllegalArgumentException(person.getName() + " menor de edad");
-    }
-
-    // Muerto
-
-    if(person.isAlive() == false) {
-      throw new IllegalArgumentException(person.getName() + " registrado como muerto");
-    }
-
-    // Duplicado
-
-
-    // Valido
-    
-    return RegisterResult.VALID;
+    return persona;
   }
+
+  // Edad Invalida
+  public RegisterResult edadRegister(Person persona) {
+
+    if(persona.getAge() <= 0) {
+      throw new IllegalArgumentException(persona.getName() + " edad invalida");
+    } else {
+      return RegisterResult.VALID;
+    }
+  }
+
+  // Menor de eddd
+  public RegisterResult menorRegister(Person persona) {
+
+    if(persona.getAge() < 18) {
+      throw new IllegalArgumentException(persona.getName() + " menor de edad");
+    } else {
+      return RegisterResult.VALID;
+    }
+  }
+
+  // Muerto
+  public RegisterResult muertoRegister(Person persona) {
+    if(persona.isAlive() == false) {
+      throw new IllegalArgumentException(persona.getName() + " registrado como muerto");
+    } else {
+      return RegisterResult.VALID;
+    }
+  }
+
 }
